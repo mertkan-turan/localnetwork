c5eeba9e08dc92952d3c7c82df1dd91b6dad38b5
mertkan-turan <mertkanturan02@gmail.com>
2023-08-07 15:10:59 +0300

    Second

--- libraries/server_tools.py
+++ libraries/server_tools.py
@@ -10,24 +10,40 @@ class Server:
        
     
     def create_server(self):
+<<<<<<< HEAD
         socket.setdefaulttimeout(25)
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.server.bind((self.ip, int(self.port)))
         self.server.listen(5)
+=======
+        socket.setdefaulttimeout(35)
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.server.bind(("localhost", int(self.port)))
+        self.server.listen(2)
+>>>>>>> 3659451 (Second)
         return self.server
 
     def server_serve(self):
         print("Server IP Address:", self.ip)
         while True:
+            conn = None
             try:
                 conn, addr = self.server.accept()
                 if conn:
                     print("Connection accepted:", addr)
                    
                     self.connection_handler(conn)
-            except Exception as error:
-                print("Connection is waiting..", error) 
+            except socket.timeout:
+                print("Connection is waiting.." +"(Timeout)")
+            except Exception as e:
+                print("Connection is waiting..", e) 
+            finally:
+                if conn:
+                    conn.close()
+                    print("Connection closed!")
+                    conn = None
 
 
     def connection_handler(self,connection):
