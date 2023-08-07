20e9761e62be235671c6e562918544eb090e8ca5
Zelal Zeynep Atabay <Zelzeyn@outlook.com>
2023-08-07 11:12:13 +0300

    pull

--- libraries/server_tools.py
+++ libraries/server_tools.py
@@ -5,7 +5,7 @@ def create_server(port):
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-    server.bind(("127.0.0.1", int(port)))
+    server.bind(("10.34.7.129", int(port)))
 
     server.listen(100)
 
--- port_scanner.py
+++ port_scanner.py
@@ -3,38 +3,36 @@ from concurrent.futures import ThreadPoolExecutor
 
 def scan_port(ip, port):
     try:
-        # Soket oluşturmak için IP versiyonunu ve türünü belirleyin (IPv4 ve TCP)
-        # Soket, AF_INET (IPv4) ve SOCK_STREAM (TCP) ile oluşturulur.
+        # Specify IP version and type to create socket (IPv4 and TCP)
+        # The socket is created with AF_INET (IPv4) and SOCK_STREAM (TCP).
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-        # 1 saniyelik bir zaman aşımı belirleyin (opsiyonel, işlem süresi için).
+       # Set a timeout of 1 second (optional, for processing time).
         sock.settimeout(5)
 
-        # Belirtilen IP adresi ve port numarasına bağlanmaya çalışın.
+       # Try to connect to the specified IP address and port number.
         print(f"Scanning {ip}:{port}")
         result = sock.connect_ex((ip, port))
-
-        # Eğer bağlantı başarılıysa (port açıksa) 0 döner.
+       # Returns 0 if the connection is successful (port is open).
         if result == 0:
             print(f"Port {port} açık.")
         
-        # Soketi kapatmayı unutmayın.
-        '#merhabalar"
-        zeynep=stajyer
+       # Do not forget to close the socket.
+      
         sock.close()
         
 
     except socket.error:
-        # Hata durumlarında (port kapalı veya hedef cihaza ulaşılamıyor) buraya düşer.
+       # It drops here in error situations (port closed or target device unreachable).
         pass
 
-# Yerel ağınızdaki cihazların IP adreslerini belirleyin.
+# Determine the IP addresses of the devices in your local network.
 local_network_ips = ['10.34.7.']
 
-# Taramak istediğiniz port numaralarını belirleyin.
-ports_to_scan = [5005]  # Örnek olarak 80, 443, 22 ve 3389 numaralı portları tarayalım.
+# Specify the port numbers you want to scan.
+ports_to_scan = [5005]  # As an example, let's scan ports 80, 443, 22 and 3389.
 
-# ThreadPoolExecutor kullanarak taramayı paralel hale getirin.
+# Parallel scanning using ThreadPoolExecutor.
 with ThreadPoolExecutor(max_workers=20) as executor:
     for ip in local_network_ips:
         for port in ports_to_scan:
