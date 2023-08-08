73f9979920d64f11c42d2948fce4e223937cf6f1
mertkan-turan <mertkanturan02@gmail.com>
2023-08-08 10:48:16 +0300

    Edit

--- libraries/client_tools.py
+++ libraries/client_tools.py
@@ -1,55 +1,37 @@
 import socket
-<<<<<<< HEAD
-import logging 
-
-logging.basicConfig(filename='my_log.log', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
-=======
 import errno
+import tkinter as tk
+from tkinter import scrolledtext
+from tkinter import messagebox
 
 class Client:
 
     def __init__(self):
-        socket.setdefaulttimeout(5)
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-    def add_message(self,message):
-        message.config(state=tk.NORMAL)
-        message.insert(tk.END, message + '\n')
-        message.config(state=tk.DISABLED)
-    
     def connect(self, ip_address, port):
-<<<<<<< HEAD
-        self.client.connect((ip_address, int(port)))
-        while True:
-            message = input("Enter a message: ")
-            self.client.sendall(message.encode())
-            logging.info(f"Message:{message}")
-
-=======
         try:
             self.client.connect((ip_address, int(port)))
             print("Successfully connected to server")
             print("If you want to exit program,please write exit!! ")
             while True:
-                
+
                 message = input("Enter a message: ")
                 self.client.sendall(message.encode())
                 if(message == "EXIT" or message == "Exit" or message == "exit"):
                     print("Are you sure you want to close the program? (Yes No)")
                     answer = input("Answer:")
                     if(answer == "Yes" or answer == "yes" or "YES"):    
+                        print("Program is closing..")
                         break
                     else:
                         continue
                     break
                 else:
                     continue
-                    
-                    
-                
-                
-            
+
+
         except socket.timeout:
             messagebox.showerror("Connection is waiting...")
             self.client.close()
@@ -63,14 +45,3 @@ class Client:
         except Exception as e:
             print("Exception:", e)
             return False
-    
-        
->>>>>>> 3659451 (Second)
-if __name__ == "__main__":
-    ip_address = "127.0.0.1"  # Example IP address
-    port = 12345  # Example port
-
-    client = Client()
-    client.connect(ip_address, port)
-
-        
