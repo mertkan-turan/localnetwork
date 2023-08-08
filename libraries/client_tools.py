d3da77f8881bb284e4775b0f3f1bac2495aed0ea
mertkan-turan <mertkanturan02@gmail.com>
2023-08-08 10:48:16 +0300

    Edit

--- libraries/client_tools.py
+++ libraries/client_tools.py
@@ -1,7 +1,14 @@
 import socket
+<<<<<<< HEAD
 import logging 
 
 logging.basicConfig(filename='my_log.log', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+=======
+import errno
+import tkinter as tk
+from tkinter import scrolledtext
+from tkinter import messagebox
+>>>>>>> 73f9979 (Edit)
 
 class Client:
 
@@ -9,6 +16,7 @@ class Client:
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
+<<<<<<< HEAD
 
     def connect(self, ip_address, port):
         self.client.connect((ip_address, int(port)))
@@ -23,3 +31,40 @@ if __name__ == "__main__":
 
     client = Client()
     client.connect(ip_address, port)
+=======
+    def connect(self, ip_address, port):
+        try:
+            self.client.connect((ip_address, int(port)))
+            print("Successfully connected to server")
+            print("If you want to exit program,please write exit!! ")
+            while True:
+
+                message = input("Enter a message: ")
+                self.client.sendall(message.encode())
+                if(message == "EXIT" or message == "Exit" or message == "exit"):
+                    print("Are you sure you want to close the program? (Yes No)")
+                    answer = input("Answer:")
+                    if(answer == "Yes" or answer == "yes" or "YES"):    
+                        print("Program is closing..")
+                        break
+                    else:
+                        continue
+                    break
+                else:
+                    continue
+
+
+        except socket.timeout:
+            messagebox.showerror("Connection is waiting...")
+            self.client.close()
+        except socket.error as e:
+            if e.errno == errno.ECONNREFUSED:
+                print("Connection Refused.")
+            else:
+                print("Socket error.", e)
+                print("Error message:", e.strerror)
+            return False
+        except Exception as e:
+            print("Exception:", e)
+            return False
+>>>>>>> 73f9979 (Edit)
