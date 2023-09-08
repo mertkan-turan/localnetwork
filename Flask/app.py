04b0c13ce22a1b395236b6c7cf0d8706b1b1af37
mertkan-turan <mertkanturan02@gmail.com>
2023-09-08 17:40:51 +0300

    Background-Change//Flask Update

--- Flask/app.py
+++ Flask/app.py
@@ -1,34 +1,31 @@
-from flask import Flask, render_template,Response
+from flask import Flask, render_template, Response
 import time
 
-app = Flask(__name__,template_folder='frontend/pages',static_folder='frontend/static')
+app = Flask(__name__, template_folder='frontend/pages', static_folder='frontend/static')
 
+# Function to stream log data
 def stream_log_data():
-    
-        with open('broadcast.log', 'r') as log_file:
-            while True:
-                message = log_file.readline()
-                if message != "":
-                    yield f"data: {message}\n\n"
-                    
+    with open('broadcast.log', 'r') as log_file:
+        while True:
+            line = log_file.readline()
+            if not line:
                 time.sleep(0.01)
-
+                continue
+            yield f"data: {line}\n\n"
 
 @app.route('/stream_log')
 def sse_log():
-    return Response(stream_log_data(), content_type='text/event-stream')    
+    return Response(stream_log_data(), content_type='text/event-stream')
 
 @app.route('/')
 def index():
-    # Read the contents of the broadcast_log file
-    with open('broadcast.log', 'r') as log_file:
-        log_contents = log_file.read()
+    try:
+        with open('broadcast.log', 'r') as log_file:
+            log_contents = log_file.readlines()
+    except FileNotFoundError:
+        log_contents = []  # Handle if the log file is not found
 
-    # Pass the log contents as a context variable to the template
     return render_template('index.html', broadcast_log=log_contents)
 
-    
-
 if __name__ == "__main__":
-    app.run(debug=True)
-    
+    app.run(host="0.0.0.0", port=8080, debug=True)
--- Flask/frontend/static/js/chat_connection.js
+++ Flask/frontend/static/js/chat_connection.js
@@ -1,11 +1,51 @@
+function appendLogMessage(element, message) {
+    const newLi = document.createElement('li'); // Create a new <li> element
+
+    // Split the message into parts based on a separator (e.g., ' - ')
+    const parts = message.split(' - ');
+
+    // Check if the message has the expected format (assuming it should have three parts)
+    if (parts.length === 3) {
+        const [ip, time, content] = parts; // Destructure the parts into variables
+
+        // Create <div> elements for sender, time-stamp, and message content
+        const newSender = document.createElement('div');
+        newSender.classList.add('sender'); // Add a CSS class
+        newSender.textContent = ip; // Set the sender's IP address
+
+        const newTime = document.createElement('div');
+        newTime.classList.add('time-stamp'); // Add a CSS class
+        newTime.textContent = time; // Set the timestamp
+
+        const newMessage = document.createElement('div');
+        newMessage.classList.add('message'); // Add a CSS class
+        newMessage.textContent = content; // Set the message content
+
+        // Append the sender, timestamp, and message content to the <li> element
+        newLi.appendChild(newSender);
+        newLi.appendChild(newMessage);
+        newLi.appendChild(newTime);
+
+        // Append the newly created <li> element to the specified HTML element
+        element.appendChild(newLi);
+
+        // Automatically scroll to the new message by setting the scrollTop property
+        element.scrollTop = element.scrollHeight;
+        
+    } else {
+        // Handle messages with unexpected format here
+        // You can choose to display them differently or log an error
+        console.error('Unexpected message format', message);
+    }
+}
 
 document.addEventListener('DOMContentLoaded', () => {
-    var logDataDiv = document.getElementById('sse-data');
+    var logDataDiv = document.getElementById('log-content');
     var eventSource = new EventSource('/stream_log');
 
     eventSource.onmessage = function (event) {
-        // Append new log data to the existing content
-        logDataDiv.innerHTML += event.data + '<br>';
-        //$('<div class="message message-personal">' + msg + '</div>').appendTo($('.mCSB_container')).addClass('new');
+        appendLogMessage(logDataDiv, event.data)
     };
+
 });
+
